c8122e16f4532c0b6b03c14c0d27c2cf83e167ea
Ngân <nganbui.23112003@gmail.com>
2025-02-23 16:24:19 +0700

    feat(home): fetch data dishlist

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,29 @@
+import dishApiRequest from '@/apiRequests/dish'
+import { DishListResType } from '@/schemaValidations/dish.schema'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  let dishList : DishListResType['data'] = []
+  try{
+    const res = await dishApiRequest.list()
+    const {payload : {data}} = res
+
+    dishList = data
+  }catch(error){
+    return <div> Try again</div>
+  }
   return (
     <div className='w-full space-y-4'>
       <div className='relative'>
-        <span className='absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10'></span>
-        {/* <Image
+        <span className='absolute top-0 left-0 w-full h-full bg-black opacity-50 '></span>
+        <Image
           src='/banner.png'
           width={400}
           height={200}
           quality={100}
           alt='Banner'
-          className='absolute top-0 left-0 w-full h-full object-cover'
-        /> */}
+          className='absolute top-0 left-0 w-full h-full object-cover z-0'
+        />
         <div className='z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20'>
           <h1 className='text-center text-white text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold'>Nhà hàng Naniri</h1>
           <p className='text-center text-sm sm:text-base mt-4 text-white'>Vị ngon, trọn khoảnh khắc</p>
@@ -21,20 +32,18 @@ export default function Home() {
       <section className='space-y-10 py-16'>
         <h2 className='text-center text-2xl font-bold'>Đa dạng các món ăn</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-10'>
-          {Array(4)
-            .fill(0)
-            .map((_, index) => (
-              <div className='flex gap-4 w' key={index}>
+          {dishList.map((dish, index) => (
+              <div className='flex gap-4 w' key={dish.id}>
                 <div className='flex-shrink-0'>
-                  <img
-                    src='https://ik.imagekit.io/freeflo/production/6b91c700-92c4-4601-8e96-37d84ac3c28c.png?tr=w-2048,q-75&alt=media&pr-true'
+                  <Image
+                    src={dish.image ?? ''} alt={dish.name} width="100" height="100"
                     className='object-cover w-[150px] h-[150px] rounded-md'
                   />
                 </div>
                 <div className='space-y-1'>
-                  <h3 className='text-xl font-semibold'>Bánh mì</h3>
-                  <p className=''>Bánh mì sandwidch</p>
-                  <p className='font-semibold'>123,123đ</p>
+                  <h3 className='text-xl font-semibold'>{dish.name}</h3>
+                  <p className=''>{dish.description}</p>
+                  <p className='font-semibold'>{dish.price}</p>
                 </div>
               </div>
             ))}
